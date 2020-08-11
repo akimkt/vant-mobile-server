87c6385952c6ccb4910563b2d13d885a4782d793
vsongshuainan <songshuainan@jd.com>
2020-08-11 11:45:50 +0800

    login

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,30 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const jwt = require('jwt-simple');
+const moment = require('moment')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('获取用户信息');
 });
 
 router.post('/login', function(req, res, next) {
-  const {username, password} = req.body
-  console.log(username, password);
-  res.send({
-    code: 200,
-    msg: '登录成功'
-  });
+  const {userName, password} = req.body
+  if(userName==='admin' && password==='123456') {
+    let expires = moment().add(1,'days').valueOf();  //设置过期时间
+    let token = jwt.encode({
+       iss: u_id,
+       exp: expires
+    }, app.get('jwtTokenSecret'));
+    res.send({
+      code: 200,
+      msg: '登录成功',
+      token
+    });
+  } else {
+    res.send({
+      code: 0,
+      msg: '用户名或密码错误'
+    });
+  }
 });
 module.exports = router;
